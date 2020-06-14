306ecb2c7d04af3e584c0262d5c603724ee7fe45
Farm Easy <farmeasy242573@gmail.com>
2020-06-14 18:36:39 +0530

    Partial Changes

---
+++ FarmEasy/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FarmEasy.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmEasy.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<UserMaster> _userManager;
+
+        public UsersController(UserManager<UserMaster> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+           var users=_userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
