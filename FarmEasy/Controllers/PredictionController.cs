f6c0e5e052d502dcc1aa669f8232ddbea9fd0316
Farm Easy <farmeasy242573@gmail.com>
2020-06-14 21:12:08 +0530

    Add DropDown In Prediction Page

--- FarmEasy/Controllers/PredictionController.cs
+++ FarmEasy/Controllers/PredictionController.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FarmEasy.Data;
+using FarmEasy.ViewModel.Prediction;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FarmEasy.Controllers
 {
     public class PredictionController : Controller
     {
-        public PredictionController()
-        {
+        private readonly ApplicationDbContext _context;
 
-        }
-        public IActionResult Index()
+        public PredictionController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = new PredictionViewModel();
+            model.City = await _context.Cities.ToListAsync();
+            model.SoilTypes = await _context.SoilTypes.ToListAsync();
+            return View(model);
         }
     }
 }
---
+++ FarmEasy/ViewModel/Prediction/PredictionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FarmEasy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmEasy.ViewModel.Prediction
+{
+    public class PredictionViewModel
+    {
+        //public City City { get; set; }
+        public int CityId { get; set; }
+        public List<City> City { get; set; }
+        public int SoilTypeId { get; set; }
+        public List<SoilType> SoilTypes { get; set; }
+    }
+}
