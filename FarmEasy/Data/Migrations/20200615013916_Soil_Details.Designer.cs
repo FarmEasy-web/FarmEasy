00f9c5f0ed3044d0de1d65e7e3eb20cf8ee87341
Farm Easy <farmeasy242573@gmail.com>
2020-06-15 08:07:05 +0530

    Add Db Scrpit and Soil Details Column

--- FarmEasy/Data/Migrations/20200615013916_Soil_Details.Designer.cs
+++ FarmEasy/Data/Migrations/20200615013916_Soil_Details.Designer.cs
@@ -4,14 +4,16 @@ using FarmEasy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FarmEasy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200615013916_Soil_Details")]
+    partial class Soil_Details
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
